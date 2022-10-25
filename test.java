70c3a57582eaf5d122f128b8a486868a8197ca95
nitro603 <tavaracristian93@gmail.com>
2022-10-25 19:17:21 -0400

    Custom format creation

--- test.java
+++ test.java
@@ -1,14 +1,15 @@
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class test{
-    public static void main(String[] args) throws ParseException{
+    public static void main(String[] args) throws ParseException,Exception{
         //Setting up objects
         HashMap<String, String> dateCollection = new HashMap<String, String>();
         Scanner stdin = new Scanner(System.in);
-
 
         Boolean running = true;
         while(running){
@@ -24,11 +25,12 @@ public class test{
             
             System.out.println("-----Menu-----");
             System.out.println("A: Add a date to collection");
-            System.out.println("B: Compare dates by input");
-            System.out.println("C: Compare dates by name in collection");
+            System.out.println("B: Compare dates by name in collection");
+            System.out.println("C: Compare dates by input");
+            
             System.out.print("Choice:");
             String letter = stdin.next();
-            option(letter, dateCollection);
+            option(letter, dateCollection,stdin);
 
             
             System.out.println("Continue? Y/N");
@@ -41,8 +43,7 @@ public class test{
         stdin.close();
     }
     
-    public static void option(String letter, HashMap<String,String> collection){
-        Scanner in = new Scanner(System.in);
+    public static void option(String letter, HashMap<String,String> collection, Scanner in) throws Exception{
         if(letter.equals("A")){
             System.out.print("Add a Date: ");
             String date = in.next();
@@ -66,18 +67,45 @@ public class test{
             System.out.println("Create your Dates");
             System.out.print("Date 1: ");
             String Date1 = in.next();
-            System.out.print("Date 2:");
+            System.out.print("Date 2: ");
             String Date2 = in.next();
-            System.out.println("The dates are " + daysBetweenDates(Date1, Date2) + " days apart");
+            int days = daysBetweenDates(Date1, Date2);
+            if(days == -1){
+                System.out.println("ERROR: please input with format MM-dd-YYYY");
+            }else{
+                System.out.println("The dates are " + days + " days apart");
+            }
         }else{
             System.out.println("ERROR: Please input A , B , C");
         }
-        in.close();
     }
 
 
-    public static int daysBetweenDates(String date1, String date2) {
-	    return Math.abs((int)ChronoUnit.DAYS.between(LocalDate.parse(date1), LocalDate.parse(date2)));
+    public static int daysBetweenDates(String date1, String date2) throws Exception {
+        
+        if(!isValidDate(date1, date2)){
+            return -1;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/d/uuuu", Locale.ENGLISH);
+        LocalDate one = LocalDate.parse(date1, formatter);
+        LocalDate two = LocalDate.parse(date2, formatter);
+
+	    return Math.abs((int)ChronoUnit.DAYS.between(one, two));
+    }
+
+    public static boolean isValidDate(String date1, String date2) throws Exception{
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/DD/YYYY");
+        try {
+            dateFormat.parse(date1.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        try {
+            dateFormat.parse(date2.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
     }
 
    
